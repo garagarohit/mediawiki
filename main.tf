a64c1c2f247cdaf9aa3f2a2b1a7c62d0e2a911a8
garagarohit <86832084+garagarohit@users.noreply.github.com>
2024-01-15 12:16:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,53 +11,10 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-#Security Group for our application
-resource "aws_security_group" "mysecuritygroup" {
-  name = "mysecuritygroup"
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-    egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "Terraform-SG"
-  }
-}
 
 resource "aws_instance" "Mediawiki" {
   ami           = "ami-04708942c263d8190"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.mysecuritygroup.id]
   key_name = "sai_devops"
 
 
