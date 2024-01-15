82e74009f348fc74d6c0fd0585e65d6b892b2b58
garagarohit <86832084+garagarohit@users.noreply.github.com>
2024-01-15 15:20:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,17 +16,11 @@ resource "aws_instance" "Mediawiki" {
   ami           = "ami-04708942c263d8190"
   instance_type = "t2.micro"
   key_name = "sai_devops"
-
+  user_data = file("mediawiki.sh")
 
   tags = {
     Name = "Mediawiki"
   }
-  provisioner "local-exec" {
-    command = "chmod 600 sai_devops.pem"
-  }
-  provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key ./sai_devops.pem -i '${aws_instance.Mediawiki.public_ip},' mediawiki-playbook.yaml"
-     }
-}
+
 
 
